a7f4d0ca205cc469eb5074d26f7086ddb5f0c959
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2023-04-08 23:03:26 +0300

    completion of the first part of the task

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,60 @@
-// const fs = require('fs/promises')
+const { nanoid } = require('nanoid');
 
-const listContacts = async () => {}
+const fs = require('fs').promises;
+const path = require('path');
+const contactsPath = path.join(__dirname, './contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contactsList = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contactsList);
+}
 
-const removeContact = async (contactId) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(contact => contact.id === contactId);
+  if (contactIndex === -1) {
+     return null;
+   }
+  console.log('updateContact', contactIndex);
+  contacts[contactIndex] = {...contacts[contactIndex],...body};
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+  return contacts[contactIndex];
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContacts = {id: nanoid(21), ...body};
+  contacts.push(newContacts);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+  return newContacts;
+} 
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(contact => contact.id === contactId);
+  return contact || null;
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(contact => contact.id === contactId);
+  if (contactIndex === -1) {
+     return null;
+   }
+  console.log('removeContact', contactIndex);
+  const result = contacts.splice(contactIndex, 1);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+  return result;
+}
+
+
+
+
 
 module.exports = {
   listContacts,
-  getContactById,
-  removeContact,
-  addContact,
   updateContact,
+  addContact,
+  getContactById,
+  removeContact
 }
