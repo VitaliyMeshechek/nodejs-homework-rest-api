2c84ee38f08474022e557e0b036c075bcb066d6a
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2023-04-11 00:38:06 +0300

    Refinement of the request

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
+
+// cors
 app.use(cors())
+
+// parse application/json
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
@@ -19,7 +23,8 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({ message, });
 })
 
 module.exports = app
