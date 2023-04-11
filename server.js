5a1a6f59902f8499c83732a6f5e6a7dfda98fa5c
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2023-04-11 22:53:15 +0300

    move DB_HOST to config.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Vitalii:EVA05LygttAtQuLa@cluster0.suvr4pr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const {DB_HOST} = require('../config');
 
 mongoose.connect(DB_HOST)
   .then(() => {
